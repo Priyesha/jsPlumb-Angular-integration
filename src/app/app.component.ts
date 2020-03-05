a00461ea9b7dc354c81dd1500870dd2fe08e2cb6
Priyesha Yadav <priyesha.yadav@yash.com>
2020-03-05 19:35:56 +0530

    jsPlumb-angular-integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { jsPlumb } from 'jsplumb';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'jsPlumb';
+export class AppComponent implements AfterViewInit, OnInit {
+  title = ' Angular jsPlumb Integration';
+  jsPlumbInstance;
+  totalnodes = 5;
+  nodes = [];
+
+  ngOnInit() {
+    // this.nodes = 
+  }
+  ngAfterViewInit() {
+    this.jsPlumbInstance = jsPlumb.getInstance();
+  }
 }
+
